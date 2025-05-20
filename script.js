6201c6874b22f611efe3122724c1e143672db8a3
Deepesh-235 <adeepesh2005@gmail.com>
2025-05-20 20:38:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add background effect to navbar on scroll
+window.addEventListener('scroll', () => {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
